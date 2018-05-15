cdc1e591eb567ef40deb2a75614d7b324ae337d6
Noa Elad <noa.elad@zenpayroll.com>
2018-05-15 13:34:29 -0700

    recover gracefully

--- app/services/device_emitter.rb
+++ app/services/device_emitter.rb
@@ -1,13 +1,19 @@
 class DeviceEmitter
   # Emits a number to the device. We're currently emitting number of payroll events in last 5 minutes but this could be anything
   def self.emit!(number)
-    device = Particle.device('LightEmitter')
-    device.function('emit', number.to_s) if device.connected?
+    sendMessage('emit', number.to_s)
   end
 
   # Emits a pulse to the device. We're currently emitting every time payroll gets processed but this could be anything
   def self.g_pulse!(number)
-    device = Particle.device('LightEmitter')
-    device.function('gPulse', number.to_s) if device.connected?
+    sendMessage('gPulse', number.to_s)
+  end
+
+  def self.sendMessage(message, data)
+    begin
+      device = Particle.device('LightEmitter')
+      device.function(message, data) if device.connected?
+    rescue Particle::BadRequest
+    end
   end
 end
